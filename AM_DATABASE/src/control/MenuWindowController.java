3d11b01febffd5f0c1576454b676648ce03c815e
GeorgeU2030 <georgeu2021@gmail.com>
2022-04-24 01:03:46 -0500

    Implementation_Datasets_AMDATABASE_2.0

--- AM_DATABASE/src/control/MenuWindowController.java
+++ AM_DATABASE/src/control/MenuWindowController.java
@@ -8,8 +8,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
@@ -63,42 +67,329 @@ public class MenuWindowController implements Initializable{
 	    @FXML
 	    private ProgressIndicator pindicator;
 
+	    class thread implements Runnable{
+
+			@Override
+			public void run() {
+				{
+				for(int i=1;i<=10;i++) {
+					pbar.setProgress(i/10.0);
+					pindicator.setProgress(i/10.0);
+					try {
+						Thread.sleep(second);
+					}catch(InterruptedException ex) {
+						System.out.println("error");
+					}
+				}
+				
+			}
+			  
+		  }
+	    }
+	    //Method start for generate the database
 	    @FXML
 	    void start(ActionEvent event) throws IOException {
-         Thread th = new Thread(new hilo());
+	    	//Method that begins the progressbar-class Thread
+         Thread th = new Thread(new thread());
          th.start();
-         second=10;
+         //time
+         second=100;
          
          String amount = amountTF.getText();
-         int amountPeople = Integer.parseInt(amount);
+         long amountPeople = Long.parseLong(amount);
          FileReader readFile;
+         //read the dataset names
          readFile = new FileReader("src/data/names.csv");
 			BufferedReader textFile = new BufferedReader(readFile);
 	        String line;
 	        String fileComplete="";
 	        while((line=textFile.readLine())!=null) {
 				fileComplete+=line+",";
+				
 	        }
+	        
+	        //Arraylist for save the names and the sex
 	        ArrayList<Namesex>ns=new ArrayList<>();
 	        String[] datanames = fileComplete.split(",");
-	        for(int i=0;i<datanames.length;i=i+2) {
+	        for(int i=0;i<datanames.length;i=i+2) 
+	        {
+	        	//We create the namesex for add the arraylist and later do shuffle
 	        	Namesex names= new Namesex(datanames[i],datanames[i+1]); 
 	        	ns.add(names);
 	        }
-	        System.out.println(ns.size());
+	        //amountnamesMax is the maximun number of names in the dataset
+	      long amountNamesmax=0;
 	        if(amountPeople>6782) {
-	        	amountPeople=6782;
+	        	//if is greater than amountPeople will be the max
+	         amountNamesmax=6782;
+	        }else {
+	        	amountNamesmax=amountPeople;
 	        }
-	        int times=(amountPeople/6782)+1;
-	        for(int i=0;i<times;i++) {
+	        //times that we will repeat the algorithm
+	        long timesnames=(amountPeople/6783)+1;
+	        for(int i=0;i<timesnames;i++) {
+	        	//we do shuffle every time that we generate the maximum number of names
+	        	if(i==timesnames-1) {
+	        		amountNamesmax=amountPeople%6782;
+	        	}
 	        	Collections.shuffle(ns);
-	        	for(int j=0;j<amountPeople;j++) {
+	        	for(int j=0;j<amountNamesmax;j++) {
+	        		//we create a person with name and sex
 	        		Person person=new Person(ns.get(j).name,ns.get(j).sex);
 	        		PersonData.getPersonData().add(person);
 	        	}
 	        }
-	        
-	       
+	        FileReader readFile2;
+	         //read the dataset names
+	         readFile2 = new FileReader("src/data/lastnames.csv");
+				BufferedReader textFile2 = new BufferedReader(readFile2);
+		        String line2;
+		        String fileComplete2="";
+		        while((line2=textFile2.readLine())!=null) {
+					fileComplete2+=line2+",";
+		        }
+		        ArrayList<String>lastnamesArray=new ArrayList<>();
+		        String[]lastnamesString=fileComplete2.split(",");
+		        for(int i=0;i<lastnamesString.length;i=i+11) {
+		        	lastnamesString[i]=lastnamesString[i].substring(1,lastnamesString[i].length()-1);
+		        	lastnamesArray.add(lastnamesString[i]);
+		        }
+	
+	          long timesLnames=(amountPeople/10000)+1;
+	          long amountLnamesmax=0;
+	          if(amountPeople>9999) {
+		        	//if is greater than amountPeople will be the max
+		         amountNamesmax=9999;
+		        }else {
+		        	amountLnamesmax=amountPeople;
+		        }
+	          for(int i=0;i<timesLnames;i++) {
+	        	  if(i==timesLnames-1) {
+	        		  amountLnamesmax=amountPeople%9999;
+	        	  }
+	        	  Collections.shuffle(lastnamesArray);
+	        	  for(int j=0;j<amountLnamesmax;j++) {
+                          PersonData.getPersonData().get(j).setLastname(lastnamesArray.get(j));                           		  
+	        	  }
+	          }
+	         double age1 = amountPeople*0.19;
+	         long age0_14=(long)age1;
+	         double age2 = amountPeople*0.13;
+	         long age15_24=(long)age1;
+	         double age3 = amountPeople*0.39;
+	         long age25_54=(long)age1;
+	         double age4 = amountPeople*0.13;
+	         long age55_64=(long)age1;
+	         double age5 = amountPeople*0.16;
+	         long age65=(long)age1;
+	         int height=0;
+	         ArrayList<Integer>months=new ArrayList<>();
+	         months.add(1);
+	         months.add(2);
+	         months.add(3);
+	         months.add(4);
+	         months.add(5);
+	         months.add(6);
+	         months.add(7);
+	         months.add(8);
+	         months.add(9);
+	         months.add(10);
+	         months.add(11);
+	         months.add(12);
+	         int dayRandom=0;
+	         int ageRandom=0;
+	         int yearRandom=0;
+	     
+	         for(int i=0;i<age0_14;i++) {
+	        	
+	        	 yearRandom=(int)(Math.random()*(2022-2008)+2008);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now());
+	        	 ageRandom=age.getYears(); 
+	        	 if(ageRandom==0||ageRandom==1) {
+	        		  height=(int)(Math.random()*(70-50)+50);
+	        	 }
+	        	 if(ageRandom==2||ageRandom==3) {
+	        		  height=(int)(Math.random()*(90-70)+70);
+	        	 }
+	        	 if(ageRandom>3&&ageRandom<7) {
+	        		  height=(int)(Math.random()*(120-90)+90);
+	        	 }
+	        	 if(ageRandom>6&&ageRandom<9) {
+	        		  height=(int)(Math.random()*(138-120)+120);
+	        	 }
+	        	 if(ageRandom>8&&ageRandom<13) {
+	        		  height=(int)(Math.random()*(150-138)+138);
+	        	 }
+	        	 if(ageRandom>12&&ageRandom<15) {
+	        		  height=(int)(Math.random()*(170-140)+140);
+	        	 }
+	        	  
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }
+	         long count=age0_14+age15_24;
+	         for(int i=(int)age0_14;i<count;i++) {
+	        	
+              yearRandom=(int)(Math.random()*(2008-1998)+1998);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now());
+	        	 ageRandom=age.getYears();
+	        	 if(ageRandom==14||ageRandom==15) {
+	        		 height=(int)(Math.random()*(170-150)+150);
+	        	 }
+	        	 if(ageRandom>15&&ageRandom<18) {
+	        		  height=(int)(Math.random()*(180-150)+150);
+	        	 }
+	        	 if(ageRandom>17&&ageRandom<25) {
+	        		 height=(int)(Math.random()*(200-160)+160);
+	        	 }
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }
+	         long counta=count;
+	         count = count+age25_54;
+	         for(int i=(int)counta;i<count;i++) {
+             yearRandom=(int)(Math.random()*(1998-1968)+1968);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now());
+	        	 ageRandom=age.getYears();
+	        	 height=(int)(Math.random()*(200-160)+160);
+	        	 
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }
+	         counta=count;
+	         count =count+age55_64;
+	         for(int i=(int)counta;i<count;i++) {
+              yearRandom=(int)(Math.random()*(1968-1958)+1958);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now());
+	        	 ageRandom=age.getYears(); 
+	        	 height=(int)(Math.random()*(170-150)+150);
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }
+	         counta=count;
+	         count=count+age65;
+	         for(int i=(int)counta;i<count;i++) {
+              yearRandom=(int)(Math.random()*(1958-1932)+1932);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now()); 
+	        	 height=(int)(Math.random()*(170-150)+150);
+	        	 ageRandom=age.getYears();
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }   
+	         for(int i=(int)count;i<amountPeople;i++) {
+             yearRandom=(int)(Math.random()*(1997-1987)+1987);
+	        	 
+	        	 Collections.shuffle(months);
+	        	 if(months.get(0)==1||months.get(0)==3||months.get(0)==5||months.get(0)==7||months.get(0)==8||months.get(0)==10||months.get(0)==12) {
+	        		 dayRandom=(int)(Math.random()*(32-1)+1);
+	        	 }
+	        	 if(months.get(0)==4||months.get(0)==6||months.get(0)==9||months.get(0)==11) {
+	        		 dayRandom=(int)(Math.random()*(31-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4==0) {
+	        		 dayRandom=(int)(Math.random()*(30-1)+1);
+	        	 }
+	        	 if(months.get(0)==2&&yearRandom%4!=0) {
+	        		 dayRandom=(int)(Math.random()*(29-1)+1);
+	        	 }
+	        	 String dateString = dayRandom+"/"+ months.get(0)+"/"+yearRandom;
+	        	 
+	        	 Period age = Period.between(LocalDate.of(yearRandom, months.get(0), dayRandom), LocalDate.now());
+	        	 height=(int)(Math.random()*(200-160)+160);
+	        	 ageRandom=age.getYears();
+	             PersonData.getPersonData().get(i).setAge(ageRandom);
+	             PersonData.getPersonData().get(i).setHeight(height);
+	             PersonData.getPersonData().get(i).setBirthDate(dateString);
+	         }
+	        /* for(int i=0;i<PersonData.getPersonData().size();i++) {
+	        	 System.out.println(i+1+""+PersonData.getPersonData().get(i).getName()+" "+PersonData.getPersonData().get(i).getAge()+" "+PersonData.getPersonData().get(i).getHeight()+PersonData.getPersonData().get(i).getBirthDate());
+	         }*/
+	         
 	    }
 	    
 	    @FXML
@@ -134,26 +425,9 @@ public class MenuWindowController implements Initializable{
 			 stage2.close();
 	    }
     
-	  class hilo implements Runnable{
-
-		@Override
-		public void run() {
-			{
-			for(int i=1;i<=10;i++) {
-				pbar.setProgress(i/10.0);
-				pindicator.setProgress(i/10.0);
-				try {
-					Thread.sleep(second);
-				}catch(InterruptedException ex) {
-					System.out.println("error");
-				}
-			}
-			
-		}
-		  
-	  }
+	 
 	   
-}
+
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -177,4 +451,6 @@ public class MenuWindowController implements Initializable{
 			this.sex=sex;
 		}
 	}
+	
+	
 }
--- AM_DATABASE/src/model/Person.java
+++ AM_DATABASE/src/model/Person.java
@@ -7,11 +7,16 @@ public class Person {
 	private String name;
 	private String lastname;
 	private String sex;
+	private int age;
+	private int height;
+	private String birthDate;
 	Image image1;
 	public Person (String name,String sex) {
-		image1= new Image("flags/bd.png");
+		image1= new Image("icons/bd.png");
 		this.name=name;
 		this.sex=sex;
+		this.lastname="";
+		this.age=0;
 	}
 	public Image getImage1() {
 		return image1;
@@ -37,6 +42,25 @@ public class Person {
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	
 	
 	
 }
